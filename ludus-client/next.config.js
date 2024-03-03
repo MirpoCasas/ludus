15cd3d6fb4e8b19b6220843e0a8ea925e32c2560
MARTIN CASAS <mirpocasas@gmail.com>
2024-03-03 18:24:02 -0300

    changed cfg

--- ludus-client/next.config.js
+++ ludus-client/next.config.js
@@ -1,10 +1,14 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   images: {
+    deviceSizes: [320, 420, 768, 1024, 1200],
+    loader: "default",
+    domains: ["res.cloudinary.com"],
     remotePatterns: [
       {
         protocol: "http",
-        hostname: "localhost",
+        hostname: "127.0.0.1",
         port: "1337",
         pathname: "/uploads/**",
       },
